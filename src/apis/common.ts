6379272350255f6c6af581bb2c6f4dc1ce5436a9
songjj <songjj@zetyun.com>
2023-08-16 11:02:07 +0800

    feat:api

--- src/apis/common.ts
+++ src/apis/common.ts
@@ -9,4 +9,19 @@ export default {
             },
         })
     },
+    creatIssue(body: any) {
+        return requests('/repos/Sjj1024/DataHub/issues', {
+            method: 'post',
+            body,
+        })
+    },
+    registUser(token: string, body: any) {
+        return requests(`/repos/Sjj1024/DataHub/issues`, {
+            method: 'post',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            body,
+        })
+    },
 }
