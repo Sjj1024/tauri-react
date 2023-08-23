61e0936bc4f715a974b925eababa9d6e8aab6094
songjj <songjj@zetyun.com>
2023-08-23 11:44:37 +0800

    feat:dom

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,17 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { appWindow } from '@tauri-apps/api/window'
 import { useEffect } from 'react'
 import './index.scss'
-import LogoImg from '@/assets/images/logo.png'
+// import LogoImg from '@/assets/images/logo.png'
 import { CloseOutlined, ExpandOutlined, MinusOutlined } from '@ant-design/icons'
 import { Dropdown, Progress, Tooltip } from 'antd'
 
 export default function Layout() {
+
+  // 路由的hooks
+  const navigate = useNavigate()
+  const location = useLocation()
+
   useEffect(() => {
     // 窗口控制功能
     document
@@ -18,12 +23,21 @@ export default function Layout() {
     document
       .getElementById('titlebar-close')!
       .addEventListener('click', () => appWindow.close())
-    // API限制进度
-    document.querySelector("div.ant-progress-bg")!.innerHTML = "<span class='api-text'>API剩余：99.67%</span>"
   }, [])
 
+
+  useEffect(() => {
+    // API限制进度
+    if (location.pathname !== "/") {
+      document.querySelector("div.ant-progress-bg")!.innerHTML = "<span class='api-text'>API剩余：99.67%</span>"
+    }
+    // 查看当前路由
+    console.log("当前路由", location.pathname);
+  }, [location])
+
+
+
   // 退出登录
-  const navigate = useNavigate()
   const logout = () => {
     navigate("/", { replace: true })
   }
@@ -65,20 +79,23 @@ export default function Layout() {
       <div className="main-header" data-tauri-drag-region>
         {/* log显示 */}
         <div className="header-action" data-tauri-drag-region>
-          <img src={LogoImg} alt="" className="action-logo" data-tauri-drag-region />
+          <img src="https://cdn.staticaly.com/gh/1024huijia/QingChunMeizi@master/dochub-b.2tlycxl6mhu0.webp" alt="" className="action-logo" data-tauri-drag-region />
           {/* 接口使用显示 */}
-          <Tooltip overlayClassName="api-tip" placement="right" title="每小时可发送5000个请求, 已使用：3, 剩余：5498 恢复时间：2023/8/18">
-            <Progress percent={89} size={[200, 20]} className="api-limit" showInfo={false}></Progress>
-          </Tooltip>
+          {location.pathname === "/" ? null :
+            <Tooltip overlayClassName="api-tip" placement="right" title="每小时可发送5000个请求, 已使用：3, 剩余：5498 恢复时间：2023/8/18">
+              <Progress percent={89} size={[200, 20]} className="api-limit" showInfo={false}></Progress>
+            </Tooltip>
+          }
         </div>
         {/* 窗口控制按钮 */}
         <div data-tauri-drag-region className="titlebar">
           {/* 用户头像 */}
           <div className='user-box'>
-            <Dropdown menu={{ items }} placement="bottom" arrow>
-              <img src="https://avatars.githubusercontent.com/u/48399687?v=4" alt="" className='user-icon' />
-            </Dropdown>
-
+            {location.pathname === "/" ? null :
+              <Dropdown menu={{ items }} placement="bottom" arrow>
+                <img src="https://avatars.githubusercontent.com/u/48399687?v=4" alt="" className='user-icon' />
+              </Dropdown>
+            }
           </div>
           {/* 窗口控制 */}
           <div className="titlebar-button" id="titlebar-minimize">
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,104 +1,135 @@
 import { Form, Input, Button, message, Spin } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { UserOutlined, LockOutlined, VerifiedOutlined } from '@ant-design/icons'
 import { useNavigate } from 'react-router-dom'
 import './index.scss'
 // import logo from '@/assets/images/logo.jpg'
 import { useState } from 'react'
 
 function Login() {
-    const navigate = useNavigate()
-    const [messageApi, contextHolder] = message.useMessage()
+  const navigate = useNavigate()
+  const [messageApi, contextHolder] = message.useMessage()
 
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-    const onFinish = async (values: any) => {
-        console.log('Success:', values)
-        setLoading(true)
-        if (values.username && values.password) {
-            // 使用github token获取用户信息
-            // const res = await commonApi.getUserInfo(values.token)
-            // console.log('获取用户信息', res)
-            // if (res && (res as any).name) {
-            //     messageApi.open({
-            //         type: 'success',
-            //         content: '校验成功',
-            //     })
-            //     // 将Token存储到本地
-            //     localStorage.setItem('token', values.token)
-            //     userInfo.setUserInfo({
-            //         userName: (res as any).name,
-            //         loginName: (res as any).login,
-            //         avatarUrl: (res as any).avatar_url,
-            //     })
-            //     navigate('/', { replace: true })
-            //     setLoading(false)
-            // }
-            navigate('/notes', { replace: true })
-            setLoading(false)
-        } else {
-            messageApi.open({
-                type: 'error',
-                content: '请将输入框填写完整',
-            })
-        }
-        // navigate('/', { replace: true })
+  const onFinish = async (values: any) => {
+    console.log('Success:', values)
+    setLoading(true)
+    if (values.username && values.password) {
+      // 使用github token获取用户信息
+      // const res = await commonApi.getUserInfo(values.token)
+      // console.log('获取用户信息', res)
+      // if (res && (res as any).name) {
+      //     messageApi.open({
+      //         type: 'success',
+      //         content: '校验成功',
+      //     })
+      //     // 将Token存储到本地
+      //     localStorage.setItem('token', values.token)
+      //     userInfo.setUserInfo({
+      //         userName: (res as any).name,
+      //         loginName: (res as any).login,
+      //         avatarUrl: (res as any).avatar_url,
+      //     })
+      //     navigate('/', { replace: true })
+      //     setLoading(false)
+      // }
+      navigate('/notes', { replace: true })
+      setLoading(false)
+    } else {
+      messageApi.open({
+        type: 'error',
+        content: '请将输入框填写完整',
+      })
     }
+    // navigate('/', { replace: true })
+  }
 
-    return (
-        <div className="login-box">
-            {contextHolder}
-            <div className="login-main">
-                <div className="avatar_box">
-                    {/* <img src={logo} alt="" className="login-logo" /> */}
-                </div>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: '请输入账号' }]}
-                        className="username"
-                    >
-                        <Input
-                            prefix={
-                                <UserOutlined className="site-form-item-icon" />
-                            }
-                            placeholder="账号"
-                            size="large"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: '请输入密码' }]}
-                    >
-                        <Input
-                            prefix={
-                                <LockOutlined className="site-form-item-icon" />
-                            }
-                            type="password"
-                            placeholder="密码"
-                            size="large"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        {loading ? (
-                            <Spin className="login-form-button" />
-                        ) : (
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                className="login-form-button"
-                                size="large"
-                            >
-                                登陆
-                            </Button>
-                        )}
-                    </Form.Item>
-                </Form>
-            </div>
+  // 控制登陆还是注册
+  const [regist, setRegist] = useState(false)
+
+  return (
+    <div className="login-box" data-tauri-drag-region>
+      {contextHolder}
+      <div className="login-main">
+        <div className="avatar_box">
+          {/* <img src={logo} alt="" className="login-logo" /> */}
         </div>
-    )
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: '请输入用户名' }]}
+            className="username"
+          >
+            <Input
+              prefix={
+                <UserOutlined className="site-form-item-icon" />
+              }
+              placeholder="用户名"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input
+              prefix={
+                <LockOutlined className="site-form-item-icon" />
+              }
+              type="password"
+              placeholder="密码"
+              size="large"
+            />
+          </Form.Item>
+          {/* 注册状态下添加Token */}
+          {
+            regist ? <Form.Item
+              name="token"
+              rules={[{ required: true, message: '请输入Github Token' }]}
+            >
+              <Input
+                prefix={
+                  <VerifiedOutlined className="site-form-item-icon" />
+                }
+                type="password"
+                placeholder="Github Token"
+                size="large"
+              />
+            </Form.Item> : null
+          }
+          <div className='login-action'>
+            <span className='login-tip'>忘记密码了？</span>
+            {
+              regist ?
+                (<>
+                  <span className='login-tip' onClick={() => setRegist(false)}>获取一个Token</span>
+                  <span className='login-tip' onClick={() => setRegist(false)}>用户名登陆</span>
+                </>
+                ) :
+                <span className='login-tip' onClick={() => setRegist(true)}>注册新账号</span>
+            }
+          </div>
+          <Form.Item>
+            {loading ? (
+              <Spin className="login-form-button" />
+            ) : (
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                size="large"
+              >
+                {regist ? "注册" : "登陆"}
+              </Button>
+            )}
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 export default Login
